6189213fde9f50d39fdc7e9cd7cf174f75cfc0ce
编程鼠鼠 <codemouse179@gmail.com>
2023-01-31 02:25:58 +0300

    Update main.cpp

--- Test/BasicTest/main.cpp
+++ Test/BasicTest/main.cpp
@@ -1,9 +1,12 @@
-﻿#ifdef _MSC_VER
+﻿#define SYSTEM_STRING_CONSOLE
+#ifdef _MSC_VER
 #include "String.hpp"
 #else
 #include "../../String.hpp"
 #endif
+using namespace System;
 int main()
 {
+    StringA::WriteLine(U8("BasicTest"));
     return 0;
 }
