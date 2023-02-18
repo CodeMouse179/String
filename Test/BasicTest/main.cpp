68cd07143dcafdd237f083551c3c1d694f8e28c8
编程鼠鼠 <codemouse179@gmail.com>
2023-02-18 14:29:52 +0400

    Update main.cpp

--- Test/BasicTest/main.cpp
+++ Test/BasicTest/main.cpp
@@ -8,5 +8,19 @@ using namespace System;
 int main()
 {
     StringA::WriteLine(U8("BasicTest"));
+    StringA::WriteLine(U8("BasicTest"), 255, 25, 135);
+    const float sec = 0.05f;
+    while (true)
+    {
+        if (StringA::KeyAvailable())
+        {
+            BuiltInConsoleKey key = StringA::ReadKey();
+            if (key.CodePoint == 'a')
+            {
+                break;
+            }
+        }
+        usleep(sec * 1000 * 1000);
+    }
     return 0;
 }
